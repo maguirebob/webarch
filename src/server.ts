67786494effdf3f1913bccc6d15c617e5a5b24ef
Bob Maguire <bibo85@yahoo.com>
2025-09-08 21:45:20 -0400

    Copy views and public files to dist during build for Vercel

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const app = express();
 
 // View engine setup
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '..', 'src', 'views'));
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
